e905be573ee0d07729b9ad6bc0de8f1b33ef8b1e
RuslanDeveloped <110823883+RuslanDeveloped@users.noreply.github.com>
2022-08-20 20:47:21 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('.TitleGoesHere__slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
